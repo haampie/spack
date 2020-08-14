36cb174cbb7dc2a41d50c4573a2c880bd1adbf5a
Harmen Stoppels <harmenstoppels@gmail.com>
2020-08-14 13:26:43 +0200

    Try rocblas 3.7.x

--- var/spack/repos/builtin/packages/rocblas/package.py
+++ var/spack/repos/builtin/packages/rocblas/package.py
@@ -12,9 +12,11 @@ class Rocblas(CMakePackage):
 
     homepage = "https://github.com/ROCmSoftwarePlatform/rocBLAS/"
     url      = "https://github.com/ROCmSoftwarePlatform/rocBLAS/archive/rocm-3.5.0.tar.gz"
+    git      = "https://github.com/ROCmSoftwarePlatform/rocBLAS.git"
 
     maintainers = ['haampie']
 
+    version('develop', commit='9d981389e77950a7954e3c5405f1137d3c1a645a')
     version('3.5.0', sha256='8560fabef7f13e8d67da997de2295399f6ec595edfd77e452978c140d5f936f0')
 
     amdgpu_targets = ('all', 'gfx803', 'gfx900', 'gfx906', 'gfx908')
@@ -22,11 +24,11 @@ class Rocblas(CMakePackage):
     variant('amdgpu_target', default='all', multi=True, values=amdgpu_targets)
 
     depends_on('cmake@3:', type='build')
-    depends_on('rocm-cmake@3.5.0', type='build', when='@3.5.0')
-    depends_on('rocm-device-libs@3.5.0', type='build', when='@3.5.0')
+    depends_on('rocm-cmake@3.5.0', type='build')
+    depends_on('rocm-device-libs@3.5.0', type='build')
 
-    depends_on('hip@3.5.0', when='@3.5.0')
-    depends_on('comgr@3.5.0', type='build', when='@3.5.0')
+    depends_on('hip@3.5.0')
+    depends_on('comgr@3.5.0', type='build')
 
     depends_on('python', type='build')
     depends_on('py-virtualenv', type='build')
@@ -37,12 +39,18 @@ class Rocblas(CMakePackage):
     # Tensile uses LLVM
     depends_on('llvm-amdgpu')
 
-    resource(name='tensile',
-            url='https://github.com/ROCmSoftwarePlatform/Tensile/archive/rocm-3.5.0.tar.gz',
-            sha256='71eb3eed6625b08a4cedb539dd9b596e3d4cc82a1a8063d37d94c0765b6f8257',
+    resource(name='Tensile',
+            git='https://github.com/ROCmSoftwarePlatform/Tensile.git',
+            commit='f842a1a4427624eff6cbddb2405c36dec9a210cd',
             when='@3.5.0')
 
+    resource(name='Tensile',
+             git='https://github.com/ROCmSoftwarePlatform/Tensile.git',
+             commit='af71ea890a893e647bf2cf4571a90297d65689ca',
+             when='@develop')
+
     patch('0001-Fix-compilation-error-with-StringRef-to-basic-string.patch')
+    patch('0002-Define-the-Tensile_LIBRARY_FORMAT-variable.patch', when='@develop')
 
     def setup_build_environment(self, env):
         env.set('CXX', self.spec['hip'].hipcc)
@@ -50,10 +58,7 @@ class Rocblas(CMakePackage):
     def cmake_args(self):
         archs = ",".join(self.spec.variants['amdgpu_target'].value)
 
-        tensile = join_path(
-            self.stage.source_path,
-            'Tensile-rocm-{0}'.format(self.version)
-        )
+        tensile = join_path(self.stage.source_path, 'Tensile')
 
         args = [
             '-Damd_comgr_DIR={0}'.format(self.spec['comgr'].prefix),
