aabed6b1ca364b7e0dcb7aab2c0a493b27ada914
Peter Josef Scheibel <scheibel1@llnl.gov>
2021-07-27 10:36:59 -0700

    update bash completion

--- share/spack/spack-completion.bash
+++ share/spack/spack-completion.bash
@@ -1483,7 +1483,7 @@ _spack_python() {
 }
 
 _spack_read_external_db() {
-    SPACK_COMPREPLY="-h --help --test --file --apply-updates"
+    SPACK_COMPREPLY="-h --help --file --apply-updates"
 }
 
 _spack_reindex() {
@@ -1707,7 +1707,7 @@ _spack_undevelop() {
 _spack_uninstall() {
     if $list_options
     then
-        SPACK_COMPREPLY="-h --help -f --force -R --dependents -y --yes-to-all -a --all"
+        SPACK_COMPREPLY="-h --help -f --force -R --dependents -y --yes-to-all -a --all --origin"
     else
         _installed_packages
     fi
