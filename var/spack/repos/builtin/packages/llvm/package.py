80918a94f466a9eaa396bc000710716e80e254ce
Harmen Stoppels <me@harmenstoppels.nl>
2023-08-24 16:07:35 +0200

    fix lldb

--- var/spack/repos/builtin/packages/llvm/package.py
+++ var/spack/repos/builtin/packages/llvm/package.py
@@ -221,6 +221,7 @@ class Llvm(CMakePackage, CudaPackage):
         description="Enable code-signing on macOS",
     )
     variant("python", default=False, description="Install python bindings")
+    variant("lua", default=True, when="@11: +lldb", description="Enable lua scripting inside lldb")
     variant("version_suffix", default="none", description="Add a symbol suffix")
     variant(
         "shlib_symbol_version",
@@ -292,14 +293,21 @@ class Llvm(CMakePackage, CudaPackage):
     with when("+lldb"):
         depends_on("libedit")
         depends_on("libxml2")
-        depends_on("lua")
+        depends_on("lua@5.3", when="+lua")  # purposefully not a range
         depends_on("ncurses")
+        depends_on("py-six", when="+python")
+        depends_on("swig", when="+lua")
+        depends_on("swig", when="+python")
         depends_on("xz")
-    with when("+lldb+python"):
-        depends_on("swig")
+
+    # Use ^swig cause it's triggered by both python & lua scripting in lldb
+    with when("^swig"):
         depends_on("swig@2:", when="@10:")
         depends_on("swig@3:", when="@12:")
-        depends_on("py-six")
+        depends_on("swig@4:", when="@17:")
+        # Commits f0a25fe0b746f56295d5c02116ba28d2f965c175 and
+        # 81fc5f7909a4ef5a8d4b5da2a10f77f7cb01ba63 fixed swig 4.1 support
+        depends_on("swig@:4.0", when="@:15")
 
     # gold support, required for some features
     depends_on("binutils+gold+ld+plugins+headers", when="+gold")
@@ -802,13 +810,15 @@ class Llvm(CMakePackage, CudaPackage):
 
         if "+lldb" in spec:
             projects.append("lldb")
-            cmake_args.extend([
-                define("LLDB_ENABLE_LIBEDIT", True),
-                define("LLDB_ENABLE_CURSES", True),
-                define("LLDB_ENABLE_LIBXML2", True),
-                define("LLDB_ENABLE_LUA", True),
-                define("LLDB_ENABLE_LZMA", True),
-            ])
+            cmake_args.extend(
+                [
+                    define("LLDB_ENABLE_LIBEDIT", True),
+                    define("LLDB_ENABLE_CURSES", True),
+                    define("LLDB_ENABLE_LIBXML2", True),
+                    from_variant("LLDB_ENABLE_LUA", "lua"),
+                    define("LLDB_ENABLE_LZMA", True),
+                ]
+            )
             if spec["ncurses"].satisfies("+termlib"):
                 cmake_args.append(define("LLVM_ENABLE_TERMINFO", True))
             else:
