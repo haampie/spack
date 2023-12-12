851595a9a4fc4e3d411685a45c2910dc63dc341f
Harmen Stoppels <me@harmenstoppels.nl>
2023-12-12 17:31:46 +0100

    undo unrelated changes

--- var/spack/repos/builtin/packages/clingo-bootstrap/package.py
+++ var/spack/repos/builtin/packages/clingo-bootstrap/package.py
@@ -108,9 +108,9 @@ class ClingoBootstrap(Clingo):
 
         # Set PGO training flags.
         generate_mods = EnvironmentModifications()
-        generate_mods.append_flags("CFLAGS", f"-fprofile-generate={reports}")
-        generate_mods.append_flags("CXXFLAGS", f"-fprofile-generate={reports}")
-        generate_mods.append_flags("LDFLAGS", f"-fprofile-generate={reports} --verbose")
+        generate_mods.append_flags("CFLAGS", "-fprofile-generate={}".format(reports))
+        generate_mods.append_flags("CXXFLAGS", "-fprofile-generate={}".format(reports))
+        generate_mods.append_flags("LDFLAGS", "-fprofile-generate={} --verbose".format(reports))
 
         with working_dir(self.build_directory, create=True):
             cmake(*cmake_options, sources, extra_env=generate_mods)
@@ -136,10 +136,10 @@ class ClingoBootstrap(Clingo):
             # merge reports
             use_report = join_path(reports, "merged.prof")
             raw_files = glob.glob(join_path(reports, "*.profraw"))
-            llvm_profdata("merge", f"--output={use_report}", *raw_files)
-            use_flag = f"-fprofile-instr-use={use_report}"
+            llvm_profdata("merge", "--output={}".format(use_report), *raw_files)
+            use_flag = "-fprofile-instr-use={}".format(use_report)
         else:
-            use_flag = f"-fprofile-use={reports}"
+            use_flag = "-fprofile-use={}".format(reports)
 
         # Set PGO use flags for next cmake phase.
         use_mods = EnvironmentModifications()
